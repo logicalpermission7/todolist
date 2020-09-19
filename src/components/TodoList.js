23edf5b373ec1542d222dce8cb2fc5fd7d7e0051
Iron Man <ironman@irons-mbp.attlocal.net>
2020-09-18 22:13:24 -0500

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+//import components
+import Todo from './Todo';
+
+function TodoList({todos, setTodos, filteredTodos}){
+
+
+
+
+
+    return(
+    <div className="todo-container">
+      <ul className="todo-list">
+      {filteredTodos.map((todo) =>(
+          <Todo 
+          todo={todo} 
+          todos={todos} 
+          setTodos={setTodos} 
+          key={todo.id} 
+          text={todo.text}/>
+      ))}
+      
+      </ul>
+      
+    </div>
+    );
+}
+
+export default TodoList;
